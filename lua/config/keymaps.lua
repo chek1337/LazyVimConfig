93990c05ac94d5ec2faa9983a8e817eb2fa915ca
Loychenko Danila <loychenko.d@yandex.ru>
2026-01-12 00:55:02 +0700

    Some improvement

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode with jj" })
+vim.keymap.set({ "n", "v" }, "q", "<Nop>", { desc = "Disable macros completely" })
+
 vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Move left" })
 vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Move down" })
 vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Move up" })
