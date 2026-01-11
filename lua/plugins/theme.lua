d684fd886bfa9d48d1ccfe1eaf2a8e5d34eb1a71
Loychenko Danila <loychenko.d@yandex.ru>
2026-01-12 00:42:26 +0700

    Change colortheme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      flavour = "mocha",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
