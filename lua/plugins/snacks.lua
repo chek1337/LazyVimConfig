9369f314179588e574d2e009ff0236e38eaf0907
Loychenko Danila <loychenko.d@yandex.ru>
2026-01-14 04:54:34 +0700

    fix pickers dropdown

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,57 +1,53 @@
 return {
   "folke/snacks.nvim",
-  opts = {
-    -- snacks.nvim/lua/snacks/picker/config/layouts.lua
-    picker = {
-      layout = {
+  priority = 10,
+  config = function()
+    local opts = {
+      dashboard = { enabled = true },
+      picker = {
         layout = {
-          backdrop = false,
-          width = 0.95,
-          min_width = 120,
-          height = 0.99,
-          border = "none",
+          preset = "dropdown",
+        },
+      },
+    }
+    require("snacks").setup(opts)
+    local layouts = require("snacks.picker.config.layouts")
+    layouts.dropdown = vim.tbl_deep_extend("force", layouts.dropdown, {
+      layout = {
+        backdrop = false,
+        row = 0,
+        width = 0.95,
+        min_width = 120,
+        height = 0.99,
+        border = "none",
+        box = "vertical",
+        -- preview
+        {
+          win = "preview",
+          title = "{preview}",
+          height = 0.7,
+          border = true,
+        },
+        {
           box = "vertical",
-          -- Preview
-          {
-            win = "preview",
-            title = "{preview}",
-            height = 0.7,
-            border = true,
-          },
-          {
-            box = "vertical",
-            border = true,
-            title = "{title} {live} {flags}",
-            title_pos = "center",
-            { win = "input", height = 1, border = "bottom" },
-            { win = "list", border = "none" },
-          },
+          border = true,
+          title = "{title} {live} {flags}",
+          title_pos = "center",
+          { win = "input", height = 1, border = "bottom" },
+          { win = "list", border = "none" },
         },
       },
-      sources = {
-        explorer = {
-          layout = {
-            layout = {
-              backdrop = false,
-              width = 40,
-              min_width = 40,
-              height = 0,
-              position = "left",
-              border = "none",
-              box = "vertical",
-              {
-                win = "input",
-                height = 1,
-                border = true,
-                title = "{title} {live} {flags}",
-                title_pos = "center",
-              },
-              { win = "list", border = "none" },
-              { win = "preview", title = "{preview}", height = 0.4, border = "top" },
-            },
-          },
-        },
-      },
-    },
-  },
+    })
+  end,
 }
+
+-- return { -- simple solution to change pickers
+--   "folke/snacks.nvim",
+--   opts = {
+--     picker = {
+--       layout = {
+--         preset = "ivy_split", -- :help snacks-picker (9. Layouts)
+--       },
+--     },
+--   },
+-- }
