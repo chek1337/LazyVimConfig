fa7a33f2ccf5cc3ca43a8833c6c3680e3152eef9
chek1337 <DaniPlay1337@yandex.ru>
2026-01-15 14:15:39 +0700

    x -> _x

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,5 @@ vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Move left" })
 vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Move down" })
 vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Move up" })
 vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Move right" })
+
+vim.keymap.set({ "n", "v" }, "x", '"_x')
