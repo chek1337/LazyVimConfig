625a9a86b2394a82708db133535bd456c93657d6
chek1337 <DaniPlay1337@yandex.ru>
2026-01-16 12:09:22 +0700

    add neogit support

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,21 @@
+return {
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "folke/snacks.nvim",
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+  },
+  opts = {
+    graph_style = "kitty",
+    integrations = {
+      telescope = false,
+      snacks = true,
+      diffview = true,
+    },
+  },
+}
