2376b8bebb23f5a3894a4f41f7f8bdff39ba39d0
chek1337 <DaniPlay1337@yandex.ru>
2026-01-16 20:05:35 +0700

    python formatter support

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
     formatters_by_ft = {
       c = { "clang-format" },
       cpp = { "clang-format" },
+      python = { "black" },
       json = { "jq" },
       jsonc = { "jq" }, -- если используете json с комментариями
     },
