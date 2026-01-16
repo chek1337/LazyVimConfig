118ad4de8313235f1325ac1c785e0496042ce02b
chek1337 <DaniPlay1337@yandex.ru>
2026-01-16 11:05:08 +0700

    Simplify layouts config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -35,35 +35,19 @@ return {
     local layouts = require("snacks.picker.config.layouts")
     layouts.dropdown = vim.tbl_deep_extend("force", layouts.dropdown, {
       layout = {
-        backdrop = false,
         row = 0,
         width = 0.95,
         min_width = 120,
         height = 0.99,
-        border = "none",
-        box = "vertical",
-        -- preview
         {
-          win = "preview",
-          title = "{preview}",
           height = 0.7,
-          border = true,
-        },
-        {
-          box = "vertical",
-          border = true,
-          title = "{title} {live} {flags}",
-          title_pos = "center",
-          { win = "input", height = 1, border = "bottom" },
-          { win = "list", border = "none" },
         },
       },
     })
     layouts.select = vim.tbl_deep_extend("force", layouts.select, {
       layout = {
-        width = 0.95, -- Увеличил ширину, чтобы сделать шире (как в dropdown)
-        min_width = 120, -- Увеличил минимальную ширину
-        max_width = nil, -- Убрал максимум или можно установить выше, если нужно
+        width = 0.95,
+        min_width = 120,
       },
     })
   end,
