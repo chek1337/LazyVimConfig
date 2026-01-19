1570b2a3a8610ee0549ec0f10e60e0b4de3ee965
chek1337 <DaniPlay1337@yandex.ru>
2026-01-19 12:15:29 +0700

    Add vim-visual-multi.nvim

---
+++ lua/plugins/vim-visual-multi.lua
@@ -0,0 +1,12 @@
+return {
+  "mg979/vim-visual-multi",
+  branch = "master",
+  lazy = false,
+  init = function()
+    vim.g.VM_maps = {
+      ["Add Cursor Up"] = "<M-Up>",
+      ["Add Cursor Down"] = "<M-Down>",
+    }
+    vim.g.VM_default_mappings = 0
+  end,
+}
