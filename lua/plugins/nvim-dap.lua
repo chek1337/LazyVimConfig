c61792476b1c476b53877ea50da633623f10941d
chek1337 <DaniPlay1337@yandex.ru>
2026-01-20 19:19:37 +0700

    Add nvim-dap-view

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -30,6 +30,7 @@ return {
     "mfussenegger/nvim-dap",
     dependencies = {
       "rcarriga/nvim-dap-ui",
+      "igorlfs/nvim-dap-view",
       {
         "mfussenegger/nvim-dap-python",
         -- stylua: ignore
@@ -64,7 +65,7 @@ return {
       { "<leader>dk", function() require("dap").up() end, desc = "Up Stack" },
       { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
       { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
-      { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Debug Widgets" },
+      { "<leader>dW", function() require("dap.ui.widgets").hover() end, desc = "Debug Widgets" },
     },
 
     config = function()
@@ -92,7 +93,57 @@ return {
     end,
   },
 
-  -- fancy UI for the debugger
+  {
+    "igorlfs/nvim-dap-view",
+    -- stylua: ignore
+    keys = {
+      {
+        "<leader>dd", 
+        function()
+          local view = require("dap-view")
+          -- Проверяем, существует ли и видим ли хотя бы один из окон плагина
+          local wins = vim.api.nvim_list_wins()
+          local has_view = false
+
+          for _, win in ipairs(wins) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            local name = vim.api.nvim_buf_get_name(buf)
+            if name:match("dap%-view") or name:match("dap%-console") then
+              has_view = true
+              break
+            end
+          end
+
+          if has_view then
+            view.close({ bang = true })
+          else
+            view.open()
+          end
+        end,
+        desc = "Debug: Toggle dap-view (open / close + term)",
+      },
+      { "<leader>dw", "<Cmd>DapViewWatch<CR>", desc = "Debug: Add Variable to Scope" },
+    },
+    opts = {
+      winbar = {
+        sections = { "watches", "scopes", "breakpoints", "repl", "exceptions", "threads", "sessions" },
+        controls = {
+          enabled = true,
+          position = "right",
+        },
+      },
+      windows = {
+        size = 0.35,
+        position = "below",
+        terminal = {
+          size = 0.4,
+          position = "right",
+        },
+      },
+      auto_toggle = true,
+    },
+  },
+
   {
     "rcarriga/nvim-dap-ui",
     dependencies = { "nvim-neotest/nvim-nio" },
