940417463c08a90597f515abfb41061003ac942a
chek1337 <DaniPlay1337@yandex.ru>
2026-01-21 17:27:46 +0700

    Refactor snacks and change picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,17 +1,16 @@
 return {
   "folke/snacks.nvim",
   priority = 10,
-  config = function()
-    local opts = {
-      notifier = {
-        timeout = 5000,
-        style = "minimal",
-      },
-      dashboard = {
-        enabled = true,
-        preset = {
-          -- https://github.com/MaximilianLloyd/ascii.nvim/blob/master/lua/ascii/text/neovim.lua
-          header = [[
+  opts = {
+    notifier = {
+      timeout = 5000,
+      style = "minimal",
+    },
+    dashboard = {
+      enabled = true,
+      preset = {
+        -- https://github.com/MaximilianLloyd/ascii.nvim/blob/master/lua/ascii/text/neovim.lua
+        header = [[
 
                                                                     
       ████ ██████           █████      ██                     
@@ -21,50 +20,51 @@ return {
    █████████ ██████████ █████████ █████ █████ ████ █████   
  ███████████ ███    ███ █████████ █████ █████ ████ █████  
 ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-        },
-        sections = {
-          { section = "header" },
-          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-          { section = "startup" },
-        },
       },
-      picker = {
-        layout = {
-          preset = "dropdown",
-        },
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
       },
-      indent = {},
-      scope = {},
-      quickfile = {},
-      scroll = {
-        animate = {
-          duration = { step = 10, total = 200 },
-          easing = "outExpo", --  outExpo, outInExpo, outBack, outInExpo
-        },
-      },
-    }
-    require("snacks").setup(opts)
-    local layouts = require("snacks.picker.config.layouts")
-    layouts.dropdown = vim.tbl_deep_extend("force", layouts.dropdown, {
-      layout = {
-        row = 0,
-        width = 0.95,
-        min_width = 120,
-        height = 0.99,
-        {
-          height = 0.7,
+    },
+    picker = {
+      layout = "reverse_dropdown",
+      layouts = {
+        reverse_dropdown = {
+          layout = {
+            backdrop = false,
+            row = 0,
+            width = 0.95,
+            min_width = 120,
+            height = 0.99,
+            border = "none",
+            box = "vertical",
+            {
+              box = "vertical",
+              border = true,
+              title = "{title} {live} {flags}",
+              title_pos = "center",
+              { win = "input", height = 1, border = "bottom" },
+              { win = "list", border = "none" },
+            },
+            { win = "preview", title = "{preview}", height = 0.7, border = true },
+          },
         },
       },
-    })
-    layouts.select = vim.tbl_deep_extend("force", layouts.select, {
-      layout = {
-        width = 0.95,
-        min_width = 120,
+    },
+    indent = {},
+    scope = {},
+    quickfile = {},
+    scroll = {
+      animate = {
+        duration = { step = 10, total = 200 },
+        -- https://github.com/kikito/tween.lua?tab=readme-ov-file#easing-functions
+        easing = "outExpo", --  outExpo, outInExpo, outBack, outInExpo
       },
-    })
-  end,
+    },
+  },
 }
 
 -- return { -- simple solution to change pickers
