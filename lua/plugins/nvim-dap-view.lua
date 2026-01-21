74a274945c69095ed4e40bcaddd817e267c7d5df
chek1337 <DaniPlay1337@yandex.ru>
2026-01-21 16:01:19 +0700

    Change keys for Debug

--- lua/plugins/nvim-dap-view.lua
+++ lua/plugins/nvim-dap-view.lua
@@ -1,34 +1,30 @@
+vim.g.dap_view_open = false
+
 return {
   "igorlfs/nvim-dap-view",
-    -- stylua: ignore
-    keys = {
-      {
-        "<leader>dd", 
-        function()
-          local view = require("dap-view")
-          -- Проверяем, существует ли и видим ли хотя бы один из окон плагина
-          local wins = vim.api.nvim_list_wins()
-          local has_view = false
-
-          for _, win in ipairs(wins) do
-            local buf = vim.api.nvim_win_get_buf(win)
-            local name = vim.api.nvim_buf_get_name(buf)
-            if name:match("dap%-view") or name:match("dap%-console") then
-              has_view = true
-              break
-            end
-          end
-
-          if has_view then
-            view.close({ bang = true })
-          else
-            view.open()
-          end
-        end,
-        desc = "Debug: Toggle dap-view (open / close + term)",
-      },
-      { "<leader>dw", "<Cmd>DapViewWatch<CR>", desc = "Debug: Add Variable to Scope" },
+  keys = {
+    {
+      "<leader>dd",
+      function()
+        if vim.g.dap_view_open then
+          vim.cmd("DapViewClose!")
+          vim.g.dap_view_open = false
+        else
+          vim.cmd("DapViewOpen")
+          vim.g.dap_view_open = true
+        end
+      end,
+      desc = "Debug: Toggle dap-view (open / close + term)",
     },
+    {
+      "<leader>dx",
+      function()
+        vim.cmd("DapViewClose!")
+      end,
+      desc = "Debug: Close dap-view force",
+    },
+    { "<leader>dw", "<Cmd>DapViewWatch<CR>", desc = "Debug: Add Variable to Scope" },
+  },
   opts = {
     winbar = {
       sections = { "watches", "scopes", "breakpoints", "repl", "exceptions", "threads", "sessions" },
