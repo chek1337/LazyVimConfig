8419a056ba024a365bdd8a9b3513d7c2a4fcc787
chek1337 <DaniPlay1337@yandex.ru>
2026-01-22 21:24:15 +0700

    disable cursorline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.cursorline = false
+
 vim.g.lazyvim_check_order = false
 
 -- vim.opt.diffopt = vim.opt.diffopt + "iwhite"
