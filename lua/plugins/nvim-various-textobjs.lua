7e14c72afa6efd2cdad0dcb5ae04233afc4cf32f
chek1337 <DaniPlay1337@yandex.ru>
2026-01-23 22:28:38 +0700

    Fix compatibility with flash (S in O-mode)

--- lua/plugins/nvim-various-textobjs.lua
+++ lua/plugins/nvim-various-textobjs.lua
@@ -6,4 +6,10 @@ return {
       useDefaults = true,
     },
   },
+  config = function(_, opts)
+    require("various-textobjs").setup(opts)
+    vim.keymap.set({ "o", "x" }, "gR", function()
+      require("various-textobjs").restOfIndentation()
+    end, { desc = "restOfIndentation textobj" })
+  end,
 }
