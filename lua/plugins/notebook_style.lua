0e77b86fabcab38016528bc805207ade5e5054b1
chek1337 <DaniPlay1337@yandex.ru>
2026-01-25 23:33:37 +0700

    More longer cells

--- lua/plugins/notebook_style.lua
+++ lua/plugins/notebook_style.lua
@@ -3,6 +3,9 @@ return {
   ft = "python",
   opts = {
     border_style = "my_style",
+    cell_width_percentage = 100, -- Cell width as % of window width (1-100)
+    min_cell_width = 200, -- Minimum cell width in characters
+    max_cell_width = 200, -- Maximum cell width in characters
     hide_border_in_insert = false,
     manual_render = false,
     colors = {
