8fbb2859b696c48da4fe741cb0107edfe5f04d81
chek1337 <DaniPlay1337@yandex.ru>
2026-01-28 18:30:58 +0700

    Tmp solution for context highlight

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -12,9 +12,18 @@ return {
       silent = true,
       desc = "Jump to context",
     },
-    opts = {
-      enable = true,
-      mode = "cursor", -- 'cursor'/'topline'
-    },
   },
+  opts = {
+    enable = true,
+    mode = "cursor", -- 'cursor'/'topline'
+  },
+  config = function(_, opts)
+    require("treesitter-context").setup(opts)
+
+    -- vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#181825" })
+    -- vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { fg = "#45475a" })
+    --
+    -- vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "Grey" })
+    -- vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { underline = true, sp = "Grey" })
+  end,
 }
